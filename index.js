96bd67b8a9b209513981080bb642cb028d30e386
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-03-14 22:24:36 -0400

    adjust style, uncommented out js function

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// // Intersection Observer for project slide-ins
+// Intersection Observer for project slide-ins
 
-// let options = {
-//   rootMargin: "-100px 0px -100px 0px",
-// };
-// let callback = (entries, observer) => {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     } else {
-//       entry.target.classList.add("show");
-//       observer.unobserve(entry.target);
-//     }
-//   });
-// };
-// let projects = document.querySelectorAll(".project>div");
-// let observer = new IntersectionObserver(callback, options);
-// projects.forEach((project) => observer.observe(project));
+let options = {
+  rootMargin: "-100px 0px -100px 0px",
+};
+let callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+let projects = document.querySelectorAll(".project>div");
+let observer = new IntersectionObserver(callback, options);
+projects.forEach((project) => observer.observe(project));
